a8b2daa63dadf707eac0f407bd6ad4504fe4adad
DRLEdition19 <drledition19@gmail.com>
2025-01-17 22:55:26 -0300

    Update conty.sh

--- app/conty.sh
+++ app/conty.sh
@@ -2,15 +2,18 @@
 
 # Variables
 CONTY_DIR="/userdata/system/pro/app/bin"
+CONTY_DIR2="/userdata/system/pro/steam"
 CONTY_URL="https://github.com/DRLEdition19/batocera.drl/releases/download/Batocera_Appimages/conty.sh"
 CONTY_BIN="/usr/bin/conty"
 
 # Create directory if it doesn't exist
 mkdir -p "$CONTY_DIR"
+mkdir -p "$CONTY_DIR2"
 
 # Download the latest version of conty.sh
 echo "Downloading the latest version of conty..."
 curl -L -o "$CONTY_DIR/conty.sh" "$CONTY_URL"
+curl -L -o "$CONTY_DIR2/conty.sh" "$CONTY_URL"
 
 # Check if download was successful
 if [ $? -ne 0 ]; then
@@ -20,6 +23,7 @@ fi
 
 # Make conty.sh executable
 chmod +x "$CONTY_DIR/conty.sh"
+chmod +x "$CONTY_DIR2/conty.sh"
 
 # Create symlink only if it doesn't already exist
 if [ ! -L "$CONTY_BIN" ]; then
